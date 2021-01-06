28a3fb00acf33158a22a975b7a3b53c31ccb3101
Kai00757132 <74966819+Kai00757132@users.noreply.github.com>
2021-01-07 00:16:50 +0800

    1-2-htdocs

---
+++ getArticleData.php
@@ -0,0 +1,31 @@
+<?php // query_API.php
+  session_start();
+  include 'connect_db.php';
+  $connect = new mysqli($host, $user, $passwd, $database);
+  if ($connect->connect_error) die("連線失敗: " . $connect->connect_error);
+  $connect->query("SET NAMES 'utf8'");
+
+
+
+  $postId = $_SESSION['postId'];
+
+  if(!isset($_SESSION['postId'])){
+    echo 'no postId';
+    return;
+  }
+
+  $query = "SELECT * FROM poster WHERE postId = '$postId' ";
+  
+  $result =  $connect->query($query);
+  
+   $arr = array();
+   
+   while($row = $result->fetch_assoc())
+  {
+    array_push($arr,$row);
+  }
+ 
+    header("content-Type: application/json; charset=utf-8");
+    echo json_encode($arr, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
+   
+?>
