797c30022e0c7fd7da7edf9e8bd240bf70a5dc45
Kai00757132 <74966819+Kai00757132@users.noreply.github.com>
2021-01-06 23:58:09 +0800

    1-1-hotdocs

---
+++ 1-1-htdocs/article.php
@@ -0,0 +1,172 @@
+<?php include('server.php'); ?>
+<?php include('server_comment.php'); ?>
+<?php include('favorite_follow.php'); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>文章</title>
+    <link rel="stylesheet" href="article.css">
+    <script src="jquery-3.5.1.js"></script>
+    <script src='//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js'></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	
+</head>
+
+<body background="blue-snow.png">
+    <div id="d1">
+        <div id="d1-1"><span id="s1"><a href="index.html" style="text-decoration:none;color: white;">海大討論區</a></span></div>
+        <div id="d1-2">
+            <button type="button" class="butt1" id='butt1-1' onclick="javascript:location.href='post.html'"></button>
+            <button type="button" class="butt1" id='butt1-2' ></button>
+            <button type="button" class="butt1" id='butt1-3' onclick="javascript:location.href='memberSet.html'"></button>
+            <button type="button" class="butt1" id='butt1-4'></button>
+        </div>
+    </div>
+    <div id='d1-3'>
+        <table id='feature' >
+            <tr class='feature'><td id='logout' >登出</td></tr>
+            <tr class='feature'><td id='otherFeature' >功能</td></tr>
+        </table>
+    </div>
+    <div id="d2">
+        <span id="post_nickname">暱稱</span><br>
+        <span id="post_accountName">@帳號</span><br>
+        <span id="post_date">發布日期</span><br><br>
+        <span id="post_title">標題</span>
+        <span id="delete_article">
+        <form action="delete.php" method="POST">
+            <button type="submit" class="butt2" id="delete_button">
+               刪除文章
+            </button>
+				<form  method="post" action="comment.php" > 
+      	
+		<div class="post-fafo">
+		<i <?php if (userFavorited($posts['postId'])): ?>
+      		  class="fa fa-star favorite-btn"
+      	  <?php else: ?>
+      		  class="fa fa-star-o favorite-btn"
+      	  <?php endif ?>
+      	  data-id="<?php echo $posts['postId'] ?>"></i>
+			<span class="favorites">收藏文章</span>
+		
+      	&nbsp;&nbsp;&nbsp;&nbsp;
+
+      	<i 
+      	  <?php if (userFollowed($posts['postId'])): ?>
+      		  class="fa fa-heart follow-btn"
+      	  <?php else: ?>
+      		  class="fa fa-heart-o follow-btn"
+      	  <?php endif ?>
+      	  data-id="<?php echo $posts['postId'] ?>"></i>
+		  <span class="follows">追蹤作者</span>
+		</div>
+        </form>
+        </span>
+        <hr>
+        <div id="d2-2">
+        <img id="img1">
+        </div>
+        <span id="post_context">內文</span>
+		
+        <div class="posts-wrapper">
+	
+		
+   	<div class="post">
+     
+      <div class="post-info">
+	    <!-- if user likes post, style button differently -->
+      	<i <?php if (userLiked($posts['postId'])): ?>
+      		  class="fa fa-thumbs-up like-btn"
+      	  <?php else: ?>
+      		  class="fa fa-thumbs-o-up like-btn"
+      	  <?php endif ?>
+      	  data-id="<?php echo $post['postId'] ?>"></i>
+      	<span class="likes"><?php echo getLikes($posts['postId']); ?></span>
+      	
+      	&nbsp;&nbsp;&nbsp;&nbsp;
+
+	    <!-- if user dislikes post, style button differently -->
+      	<i 
+      	  <?php if (userDisliked($posts['postId'])): ?>
+      		  class="fa fa-thumbs-down dislike-btn"
+      	  <?php else: ?>
+      		  class="fa fa-thumbs-o-down dislike-btn"
+      	  <?php endif ?>
+      	  data-id="<?php echo $post['postId'] ?>"></i>
+      	<span class="dislikes"><?php echo getDislikes($posts['postId']); ?></span>
+      </div>
+   	</div>
+  
+  
+  
+        <div id="d3">
+          
+        </div>
+        <div >
+            <span id="accountName"></span>
+            <span id="commentTime"></span>
+            <span id="comment"></span>
+            <span id="likenum"></span>
+            <span id="dislikenum"></span>
+        </div>
+        <div>
+            <form  method="post" action="comment.php" >  
+                <textarea id="comment"  name="comment" placeholder="留言....."  maxlength="350"  required ></textarea>           
+               <button  type="submit" >留言</button>
+               
+            </form>
+                
+                <!--<span id = 'tbody1' name='tbody1' class='tbody1' ></span>-->
+               <div class="posts-wrapper">
+	<?php foreach ($comments as $comment): ?>       
+    <div class="post">
+        <?php echo '<hr/>@'.$comment['accountName'];?>
+        <?php echo '<br>';?>
+        <?php echo $comment['commentTime'];?>
+        <?php echo '<br>';?> 
+        <?php echo $comment['comment'];    ?>
+
+
+        <div class="post-info">
+     <!-- if user likes post, style button differently -->
+       <i <?php if (userLiked2($comment['comment_ID'])): ?>
+             class="fa fa-thumbs-up like-btn2"
+         <?php else: ?>
+             class="fa fa-thumbs-o-up like-btn2"
+         <?php endif ?>
+         data-id="<?php echo $comment['comment_ID'] ?>"></i>
+       <span class="likes2"><?php echo getLikes2($comment['comment_ID']); ?></span>
+       
+       &nbsp;&nbsp;&nbsp;&nbsp;
+
+     <!-- if user dislikes post, style button differently -->
+       <i 
+         <?php if (userDisliked2($comment['comment_ID'])): ?>
+             class="fa fa-thumbs-down dislike-btn2"
+         <?php else: ?>
+             class="fa fa-thumbs-o-down dislike-btn2"
+         <?php endif ?>
+         data-id="<?php echo $comment['comment_ID'] ?>"></i>
+       <span class="dislikes2"><?php echo getDislikes2($comment['comment_ID']); ?></span>
+   </div>
+    </div>
+
+    <?php endforeach?> 
+</div>
+       
+
+
+
+        </div>
+            
+    </div>
+            
+  <script src="scripts.js"></script>
+    <script src='article.js'></script>
+</body>
+
+</html>
+ 
---
+++ 1-1-htdocs/comment.php
@@ -0,0 +1,47 @@
+<!doctype html> 
+<html> 
+<head> 
+<meta charset="UTF-8"> 
+<title>留言</title> 
+</head> 
+<body> 
+<?php
+ session_start();
+ include 'connect_db.php';
+ $connect = new mysqli($host, $user, $passwd, $database);
+ if ($connect->connect_error) die("連線失敗: " . $connect->connect_error);
+ $connect->query("SET NAMES 'utf8'");
+
+$postId=$_SESSION['postId'];
+
+$result = $connect -> query("SELECT * FROM comment WHERE postId = '$postId'"); 
+$count = $result -> num_rows;
+
+
+if($count==0){$count='cmt001';}
+else{
+    $Result = $connect->query("select * from comment ORDER BY comment_ID DESC LIMIT 0 , 1");
+    $Result->data_seek(0);
+    $count = 'cmt'.substr((string)(substr($Result->fetch_array()['comment_ID'],-3)+1001),-3);
+}
+
+
+
+$accountName=$_SESSION['nickname'];
+$comment_ID=$count;
+$comment=$_POST['comment'];
+//$time_query="select now()";
+$time= date("Y-m-d H:i:s",mktime(date('H')+7,date('i'),date('s'),date('m'),date('d'),date('Y'))) ;//$db->query($time_query);//YYYY-MM-DD HH:MI:SS
+$likenum=0;
+$dislikenum=0;
+
+$full_query="insert into comment values('$postId','$accountName','$comment_ID','$comment','$time','$likenum','$dislikenum')"; 
+$stmt1=$connect->query($full_query);
+
+?> 
+<script type="text/javascript"> 
+alert("留言成功"); 
+window.location.href="article.php"; 
+</script> 
+</body> 
+</html>
